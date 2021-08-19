6da4072129fbc3f49b77d1b13157916ecf58ee5d
Ashfal Fuady <ashfal.fuady@gmail.com>
2021-08-19 13:04:37 +0700

    Update ES8388.cpp
    add mclk set up

--- src/ES8388.cpp
+++ src/ES8388.cpp
@@ -146,6 +146,10 @@ bool ES8388::begin(int sda, int scl, uint32_t frequency)
         res &= write_reg(ES8388_ADDR, ES8388_DACPOWER, 0x3c);
         res &= write_reg(ES8388_ADDR, ES8388_DACCONTROL3, 0x00);
         res &= write_reg(ES8388_ADDR, ES8388_ADCPOWER, 0x00);
+        
+        /* set up MCLK) */
+		PIN_FUNC_SELECT(PERIPHS_IO_MUX_GPIO0_U, FUNC_GPIO0_CLK_OUT1);
+        WRITE_PERI_REG(PIN_CTRL, 0xFFF0);
     }
 
     return res;
